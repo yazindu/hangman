1cbdf2ec31ecd07183ef590b3ad505ef2e2b0021
yasindu <yazindu@gmail.com>
2023-08-13 20:50:06 +0530

    feat: HangmanDrawing.tsx completed

--- src/App.tsx
+++ src/App.tsx
@@ -13,18 +13,18 @@ function App() {
 
     console.log(wordToGuess);
     return (<div style={{
-        maxWidth: "800px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "2rem",
-        margin: "0 auto",
-        alignItems: "center"
-    }}>
-        <div style={{fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
-    <HangmanDrawing/>
-    <HangmanWord/>
-    <Keyboard/>
-    </div>
+            maxWidth: "800px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+            margin: "0 auto",
+            alignItems: "center"
+        }}>
+            <div style={{fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
+            <HangmanDrawing/>
+            <HangmanWord/>
+            <Keyboard/>
+        </div>
     )
 }
 
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,3 +1,3 @@
 export const Keyboard = () => {
-
+    return (<p>KEYBOARD</p>)
 }
