33851ef8b605428beefab2da9bafa0e043c4bd0f
yasindu <yazindu@gmail.com>
2023-08-13 10:01:12 +0530

    feat: structure completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import words from "./wordList.json"
+import {useState} from "react";
+import {Keyboard} from "./Keyboard.tsx";
+import {HangmanWord} from "./HangmanWord.tsx";
+import {HangmanDrawing} from "./HangmanDrawing.tsx";
+
 function App() {
-    return (<h1>Hello</h1>)
+    const [wordToGuess, setWordToGuess] = useState(() => {
+        return words[Math.floor(Math.random() * words.length)]
+    })
+
+    const [guessedLetters, setguessedLetters] = useState<string[]>([])
+
+    console.log(wordToGuess);
+    return (<div style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center"
+    }}>
+        <div style={{fontSize: "2rem", textAlign: "center"}}>Lose Win</div>
+    <HangmanDrawing/>
+    <HangmanWord/>
+    <Keyboard/>
+    </div>
+    )
 }
 
 export default App
