22c2ee03cc7586bef7403192ee40ef4617dbbed6
yasindu <yazindu@gmail.com>
2023-08-13 21:17:59 +0530

    feat: HangmanWord.tsx completed

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,3 +1,22 @@
 export const HangmanWord = () => {
-    return (<p>WORD</p>)
+    const word = "test";
+    const guessedLetters = ["t", "e", "g"];
+    return (<div style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace"
+    }}>
+        {word.split("").map((letter, index) => (
+            <span style={{borderBottom: ".1em solid black"}} key={index}>
+                <span style={{visibility: guessedLetters.includes(letter)
+                        ? "visible"
+                        : "hidden"}}>
+                {letter}
+            </span>
+            </span>
+        ))}
+    </div>)
 }
